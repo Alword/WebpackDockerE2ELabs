423b6be5090e272fc6ef78034322356a009fa891
Alword <anzer987@yandex.ru>
2019-10-31 23:15:20 +0300

    feature screensaver

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import React from 'react'
 export default function App() {
     const [data, setData] = React.useState([]);
     let url = "https://api.kanye.rest/";
-    //     let response = await fetch(url);
 
     React.useEffect(() => {
-        fetch(url)
-          .then(response => response.json())
-          .then(data => {
-            setData(data.quote);
-          });
-      }, []);
-  
+        Request(setData);
+        setInterval(() => {Request(setData);}, 10000);
+    }, []);
+
     return <div>{data}</div>;
+}
+
+const Request = (setData) => {
+    let url = "https://api.kanye.rest/";
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            setData(data.quote);
+        });
 }
