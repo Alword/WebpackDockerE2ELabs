6d5bf78874f28baa8d767da6a498ade823b969b4
Alword <anzer987@yandex.ru>
2019-10-31 20:20:04 +0300

    feature html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    entry: './src/index.js',
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Lab 2 WebPack Task 16',
+    }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 };
